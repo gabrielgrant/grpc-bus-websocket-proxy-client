19dbd4b6f559b4334271ce5dae619a832265e7ad
Gabriel Grant <g@briel.ca>
2017-03-02 15:13:13 -0800

    add streaming example in demo

--- demo/demo.js
+++ demo/demo.js
@@ -11,6 +11,9 @@ new GBC("ws://localhost:8081/", 'helloworld.proto', {helloworld: {Greeter: 'loca
     gbc.services.helloworld.Greeter.sayHello({name: 'Gabriel'}, function(err, res){
       console.log(res);
     });  // --> Hello Gabriel
+    gbc.services.helloworld.Greeter.streamOutHello({name: 'Gabriel', iterations: 3}).on('data', function(data){
+      console.log(data);
+    });
   });
 
 // vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
