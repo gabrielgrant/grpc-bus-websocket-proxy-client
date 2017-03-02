61be12b0744b1c6667e03030fabb4805ec177feb
Gabriel Grant <g@briel.ca>
2017-03-02 15:13:52 -0800

    rebundle

--- demo/bundle.js
+++ demo/bundle.js
@@ -2198,6 +2198,9 @@ new GBC("ws://localhost:8081/", 'helloworld.proto', {helloworld: {Greeter: 'loca
     gbc.services.helloworld.Greeter.sayHello({name: 'Gabriel'}, function(err, res){
       console.log(res);
     });  // --> Hello Gabriel
+    gbc.services.helloworld.Greeter.streamOutHello({name: 'Gabriel', iterations: 3}).on('data', function(data){
+      console.log(data);
+    });
   });
 
 // vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
@@ -2411,7 +2414,12 @@ GBC.prototype.connect = function() {
     protoFileContents: fetchProtoFilePromise(this.protoFile),
     ws: wsPromise
   }).then(function(results) {
-    self.protoDefs = protobuf.loadProto(results.protoFileContents, null, self.protoFile);
+    var protoFileExt = self.protoFile.substr(self.protoFile.lastIndexOf('.') + 1);
+    if (protoFileExt === "json") {
+      self.protoDefs = protobuf.loadJson(results.protoFileContents, null, self.protoFile);
+    } else {
+      self.protoDefs = protobuf.loadProto(results.protoFileContents, null, self.protoFile);
+    }
     var initMessage = {
       filename: self.protoFile,
       contents: results.protoFileContents
