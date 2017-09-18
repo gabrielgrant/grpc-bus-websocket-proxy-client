58d3d3a83c16e378159cfbcff6a8d48219d9f964
Sean Jezewski <seanwjezewski@gmail.com>
2017-09-18 15:42:18 -0700

    Add streaming request to demo

--- demo/demo.js
+++ demo/demo.js
@@ -3,6 +3,7 @@
 //
 
 var GBC = require("../index.js");
+var async = require("async");
 
 new GBC("ws://localhost:8081/", 'helloworld.proto', {helloworld: {Greeter: 'localhost:50051'}})
   .connect()
@@ -14,6 +15,32 @@ new GBC("ws://localhost:8081/", 'helloworld.proto', {helloworld: {Greeter: 'loca
     gbc.services.helloworld.Greeter.streamOutHello({name: 'Gabriel', iterations: 3}).on('data', function(data){
       console.log(data);
     });
+
+    var call = gbc.services.helloworld.Greeter.streamInHello(function(error, data) {
+      if (error) {
+        console.log("error:", error);
+      }
+      console.log("data:", data);
+    });
+        
+    function nameSender(name) {
+      return function(callback) {
+        console.log("sending name:", name);
+        call.write({
+          name: name
+        });
+        callback();
+      };
+    }
+
+    firstName = nameSender('Gabriel');
+    lastName = nameSender('Grant');
+
+    async.series([firstName, lastName], function() {
+      console.log("Finished sending names ... closing call");
+      call.end();
+    });
+
   });
 
 // vim: tabstop=8 expandtab shiftwidth=2 softtabstop=2
